9f1b3f74f69cb092da65d12599daaf0c0a07b39f
Raj Thombare <rajthombare.dev@gmail.com>
2025-05-18 19:31:18 +0530

    initial commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -18,12 +18,11 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
+        <Stack.Screen name='index' options={{ headerShown: false }} />
       </Stack>
-      <StatusBar style="auto" />
+      <StatusBar style='auto' />
     </ThemeProvider>
   );
 }
